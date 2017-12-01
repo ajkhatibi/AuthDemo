dde74647ec5fe712b5cf7c4eb9593212ec62a595
Julian Khatibi <ajkhatibi@gmail.com>
2017-12-01 12:24:40 -0800

    completed auth sign in and sign out using state

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
 import React, { Component } from 'react';
 import { View, Text } from 'react-native';
 import firebase from 'firebase';
-import { Header } from './components/common';
+import { Header, Button, Spinner } from './components/common';
 import LoginForm from './components/LoginForm';
 
 export default class App extends Component {
+    constructor(){
+        super()
+        this.state = {
+            loggedIn: null
+        };
+    };
+
     componentWillMount(){
         firebase.initializeApp({
             apiKey : "AIzaSyBkFSuf-eEvEjoc7mP2FKwm4PEJBoOJWhk",
@@ -14,13 +21,33 @@ export default class App extends Component {
             storageBucket : "",
             messagingSenderId : "945857464132"
         });
+        firebase.auth().onAuthStateChanged((user)=>{
+            if(user){
+                this.setState({loggedIn: true })
+            }
+            this.setState({loggedIn: false })
+        })
+    }
+
+    renderUserState(){
+        switch (this.state.loggedIn) {
+            case true:
+                return (
+                    <Button onPress={()=>firebase.auth().signOut()}>Log Out</Button>
+                );
+            case false:
+                return <LoginForm />;
+            default:
+                return <Spinner size='large'/>;
+
+        };
     }
 
     render(){
         return (
             <View>
                 <Header title='AuthDemo' />
-                <LoginForm/>
+                {this.renderUserState()}
             </View>
         )
     }
